adfa15a930c19828438a2b5c31e69f1e731750fb
wangfei <dahaifeige@users.noreply.github.com>
2018-08-09 22:00:03 +0800

    code
    iniit code

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,32 +1,48 @@
-import sys
-
 import pygame
+from pygame.sprite import Group
+
+from settings import Settings
+from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
+from ship import Ship
+from alien import Alien
+import game_functions as gf
 
 
 def run_game():
 
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
-
-    # 设置背景色
-    bg_color = (230, 230, 230)
-
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height) )
     pygame.display .set_caption("Alien Invasion")
 
-    #开始有游戏的主循环
+    # 创建Play按钮
+    play_button = Button(ai_settings, screen, "play")
+
+    # 创建存储游戏统计信息的实例，并创建记分牌
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+    # 创建一艘飞船、一个子弹编组和一个外星人编组
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen,  ship, aliens)
+
+    # 开始有游戏的主循环
     while True:
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
 
-        # 监视键盘和鼠标事件
-        for event in  pygame.event.get():
-            if event.type == pygame.quit():
-                sys.exit()
+        if stats.game_active:
+            ship.update()
+            gf.update_bullet(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
-        #每次循环时都重绘屏幕
-        screen.fill(bg_color)
-
-        # 让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 
 run_game()
